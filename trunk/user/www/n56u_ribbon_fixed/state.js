091db1ef37ca9eca4d3ba632296f718f635e5a43
ifox6677 <zhangjq0908@outlook.com>
2024-10-10 02:07:11 +0800

    Update state.js

--- trunk/user/www/n56u_ribbon_fixed/state.js
+++ trunk/user/www/n56u_ribbon_fixed/state.js
@@ -747,7 +747,7 @@ function show_footer(){
 	footer_code +='<div align="center">\n';
 	footer_code +='  <span>\u5927\u7c73\u8def\u7528\u5668</span></br>\n';
 	footer_code +='  <span>Big icons designed by <a href="http://www.freepik.com">Freepik</a></br></span>\n';
-	footer_code +='  <span>Non-Commercial Use Only</span></br>\n';
+	footer_code +='  <span>\u56fa\u4ef6MIT\u6388\u6743\u53ea\u9650\u4e2a\u4eba\u4f7f\u7528\u4e0d\u5f97\u7528\u4e8e\u5546\u4e1a\u7528\u9014\u3002</span></br>\n';
 	footer_code +='</div>\n';
 
 	$("footer").innerHTML = footer_code;
